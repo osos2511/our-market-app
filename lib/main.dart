f32c1db0b1d407b07e2f83264c81c12bf45cb504
osama mohamed <om329211@gmail.com>
2025-06-23 14:44:24 +0300

    +initial-app

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:our_market/views/auth/ui/login_view.dart';
+
+void main() {
+  runApp(const OurMarket());
+}
+
+class OurMarket extends StatelessWidget {
+  const OurMarket({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Our Market',
+      home: const LoginView(),
+    );
+  }
+}
---
+++ lib/views/auth/ui/login_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Login View',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
